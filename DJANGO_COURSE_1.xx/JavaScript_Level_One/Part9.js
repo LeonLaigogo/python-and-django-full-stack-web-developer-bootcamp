9aaea93c81dc193129755307da66ad82b6e260af
Leon Lai <laputaca@gmail.com>
2020-02-05 23:40:07 +0800

    js level one project done!

--- DJANGO_COURSE_1.xx/JavaScript_Level_One/Part9.js
+++ DJANGO_COURSE_1.xx/JavaScript_Level_One/Part9.js
@@ -1,49 +1,27 @@
-var firstName = prompt("Hello and Welcome. Please enter your first name:");
-var lastName = prompt("Please enter your Last Name:");
+var first_name = prompt("What is your first name?");
+var last_name = prompt("What is your last name?");
 var age = prompt("How old are you?");
-var height = prompt("How tall are you in centimeters?");
-var petName = prompt("What is the name of your pet?");
-alert("Thank you so much for the information.")
+var tall = prompt("What is your height?");
+var pet_name = prompt("What is your pet's name?");
+alert("Thank you for your infomation!! :)")
 
-// Four Conditions need to be true for the spy alert!
-// We'll start with all of them being false or null
-var nameCond = null
-var ageCond = null
-var heightCond = null
-var petCond = null
+var age_lower_limit = 20;
+var age_upper_limit = 30;
+var tall_lower_limit = 170;
 
-// Spy has same first letter for first name and last name.
-if (firstName[0] === lastName[0]){
-  nameCond = true;
-}else {
-  nameCond = false;
+var is_pass_test = false;
+
+if (first_name[0] === last_name[0]) {
+  if (age > age_lower_limit && age < age_upper_limit) {
+    if (tall >= tall_lower_limit) {
+      if (pet_name[pet_name.length - 1] === "y") {
+        console.log("Congratulations! You pass the spy test!");
+        is_pass_test = true;
+      }
+    }
+  }
 }
 
-// Spy is between Age of 20 and 30 (exclusive)
-if (age > 20 && age <30){
-  ageCond = true;
-}else{
-  ageCond = false;
-}
-
-// Spy is at least 170 cm tall
-if (height >= 170){
-  heightCond = true;
-}else{
-  heightCond = false;
-}
-
-// Pet Name
-if (petName[petName.length-1] === "y"){
-  petCond = true;
-}else{
-  petCond = false;
-}
-
-// Check all four conditions
-if (nameCond && ageCond && heightCond && petCond){
-  // My secret message
-  console.log("Welcome Comrade! You've passed the Spy Test")
-}else{
-  console.log("Sorry, nothing to see here.")
+if (!is_pass_test) {
+  console.log("Nothing happened!");
 }
